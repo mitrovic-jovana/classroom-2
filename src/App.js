984b2b0657189033f18c3acd318d1da2bac4a69d
Dusan-Mojsilovic-EY <85942950+Dusan-Mojsilovic-EY@users.noreply.github.com>
2021-06-24 14:59:11 +0200

    firstCommit

--- src/App.js
+++ src/App.js
@@ -9,6 +9,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <h4>Dusan Mojsilovic</h4>
+        <p>This is test for git!</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam dolorem impedit temporibus praesentium magni facere aspernatur rem iure blanditiis minima. Impedit placeat esse ex expedita quia explicabo vitae officia eveniet!</p>
         <a
           className="App-link"
           href="https://reactjs.org"

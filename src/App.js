d6f569f42ec6f75987242035bddffb8774334853
Momir Radovanovic <Momir.Radovanovic@rs.ey.com>
2021-06-24 15:44:26 +0200

    Code formated

--- src/App.js
+++ src/App.js
@@ -2,27 +2,24 @@ import logo from './logo.svg';
 import './App.css';
 
 const App = () => (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h4> Hello Dear Class 2021 </h4> 
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Welcome to EY!!!
-        </a>
-      </header>
-<<<<<<< HEAD
-      <main>
-        <p>My name is Momir</p>
-      </main>
-=======
-      <p> Lorem ipsum dolor sit amet ... </p>
->>>>>>> develop
-    </div>
-  );
+  <div className="App">
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <h4> Hello Dear Class 2021 </h4>
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Welcome to EY!!!
+      </a>
+    </header>
+    <main>
+      <p>My name is Momir</p>
+    </main>
+    <p> Lorem ipsum dolor sit amet ... </p>
+  </div>
+);
 
 export default App;

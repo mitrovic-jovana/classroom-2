9152906ad78c00459e2ba8b6a2ec146fd1f8ad59
Damjan Bulatovic <Damjan.Bulatovic@rs.ey.com>
2021-06-24 14:59:01 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -4,21 +4,43 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      
+        <div class="form-wrap">
+            <h1>Sign Up</h1>
+            <p>It'sdlkaeaeveavaavearaev</p>
+            <form>
+                <div class="form-group">
+                    <label for="first-name">First Name</label>
+                    <input type="text" name="firstName" id="firstName">
+                </div>
+                <div class="form-group">
+                    <label for="last-name">Last Name</label>
+                    <input type="text" name="lastName" id="lastName">
+                </div>
+                <div class="form-group">
+                    <label for="email">Email</label>
+                    <input type="email" name="email" id="email">
+                </div>
+                <div class="form-group">
+                    <label for="password">Password</label>
+                    <input type="password" name="password" id="password">
+                </div>
+                <div class="form-group">
+                    <label for="password2">First name</label>
+                    <input type="password" name="password2" id="password2">
+                </div>
+                <button type="submit" class="btn">Sign Up</button>
+                <p>adkjgapiAPOIGA</p>
+            </form>
+        </div>
+        <footer>
+            <p>sdoadoadaa <a href="">Login Here</a></p>
+        </footer>
+     </div>
+    
+  
+    
+    
   );
 }
 
